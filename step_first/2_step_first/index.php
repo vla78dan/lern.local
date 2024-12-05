19ce1ecc9029ea419001d303fc2c1d495016f3b7
@phpsturm <vla78dan@gmail.com>
2024-12-05 22:49:18 +0300

    php first stepik

---
+++ step_first/2_step_first/index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+//echo " Приветствую, Степик!";
+
+//$x = readline();
+//$y = readline();
+//echo $x + $y;
+
+//define("GREETING", "Приветствую, Степик!");
+//echo GREETING;
+
+//$a = 0.6;
+//$b = 0.3;
+//
+//echo $a + $b;
+
+//$str1 = "Hello,";
+//$str2 = "Stepik!";
+//
+//echo $str1 . " " . $str2;
+
+//echo 'January\nFebrary\nMarch';
+//echo "January\nFebrary\nMarch";
+
+echo "Январь\nФевраль\nМарт";
+
+
+
+
+
+
+
+
+?>
+
+<h3>Stepik</h3>
+</body>
+</html>
+
+
+
