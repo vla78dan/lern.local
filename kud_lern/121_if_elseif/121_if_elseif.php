2a25582634b3ff4858c058516e555cf4cf3b7b39
@phpsturm <vla78dan@gmail.com>
2024-12-15 12:43:10 +0300

    Kud_php_12.1end

---
+++ kud_lern/121_if_elseif/121_if_elseif.php
@@ -0,0 +1,47 @@
+<?php error_reporting(-1);
+
+/*echo ' test1 ', ' test2 ', ' test3 ';
+print ' test1 ';
+
+var_dump(print 'test') ;
+var_dump(echo 'test') ;
+
+
+$light = 'green';
+
+if ($light === 'green') {
+    echo 'GO!';
+} else{
+    echo 'Stop!';
+}
+
+
+$light = 'green';
+if ($light == 'red')
+    echo 'Stop!';
+    echo 'Wait';
+*/
+
+$light = 'yellow';
+if ($light == 'green') {
+    echo 'GO!';
+} elseif ($light == 'yellow') {
+    echo 'WAIT!!!!';
+} else {
+    echo 'STOP!!!!';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
