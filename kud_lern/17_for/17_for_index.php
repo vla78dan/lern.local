7bbd5bec80b455b74ffd88c3e872365fd488f691
@phpsturm <vla78dan@gmail.com>
2024-12-17 17:34:33 +0300

    kudlay_17_for

---
+++ kud_lern/17_for/17_for_index.php
@@ -0,0 +1,46 @@
+<?php error_reporting(E_ALL);
+
+
+//$i = 1;
+//while ($i <= 10) {
+//    echo "{$i}<br>";
+//    $i++;
+//}
+
+//for ($i = 20; $i >= 1; $i--) {
+//    echo "{$i}<br>";
+//}
+echo "<select>";
+for ($i = 1987; $i <= 2024; $i++) {
+    echo "<option value='$i'>$i</option>";
+}
+echo "</select>";
+
+echo '<table border = "1" width = "100%"';
+
+$tr = 1;
+while ($tr < 10) {
+    echo "<tr>";
+    $td = 1;
+    while ($td < 10) {
+        echo "<td>$td * $tr =" . $td * $tr . "</td>";
+        $td++;
+    }
+    echo "</tr>";
+    $tr++;
+}
+echo '<table>';
+
+echo '<table border = "1" width = "100%"';
+
+
+for ($tr = 1; $tr < 10; $tr++) {
+    echo "<tr>";
+    for ($td = 1; $td < 10; $td++) {
+        echo "<td>$td * $tr =" . $td * $tr . "</td>";
+    }
+    echo "</tr>";
+}
+echo '<table>';
+
+?>
