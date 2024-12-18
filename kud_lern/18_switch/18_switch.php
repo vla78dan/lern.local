394b46727e88d9fc06254eade7d2a63f747b7745
@phpsturm <vla78dan@gmail.com>
2024-12-18 17:40:00 +0300

    kudlay_18_switch

---
+++ kud_lern/18_switch/18_switch.php
@@ -0,0 +1,54 @@
+<?php error_reporting(E_ALL);
+
+
+
+//if ($i == 1) {
+//    echo 1;
+//} elseif ($i == 2) {
+//    echo 2;
+//} elseif ($i == 3) {
+//    echo 3;
+//} elseif ($i == 4) {
+//    echo 4;
+//} else {
+//    echo 5;
+//}
+
+//switch ($i)
+//{
+//    case 1:
+//        echo 1;
+//        break;
+//    case 2:
+//        echo 2;
+//        break;
+//    case 3:
+//        echo 3;
+//        break;
+//    case 4:
+//        echo 4;
+//        break;
+//    default:
+//        echo 5;
+//
+//}
+$i = 5;
+
+
+
+switch ($i)
+{
+    case 1:
+
+    case 2:
+
+    case 3:
+        echo "Переменная < 4";
+        break;
+    case 4:
+        echo 4;
+        break;
+    default:
+        echo "Переменная > 4";
+
+}
