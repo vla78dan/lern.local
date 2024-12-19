910454e53654325ea371d422053cac073fbf7e74
@phpsturm <vla78dan@gmail.com>
2024-12-19 16:29:51 +0300

    kudlay_21_array

--- kud_lern/21_arrays/21_arrays.php
+++ kud_lern/21_arrays/21_arrays.php
@@ -1,19 +1,65 @@
 <?php error_reporting(E_ALL);
+/*
+$arr2 = [];
+var_dump($arr, $arr2);
+var_dump($arr1);
+print_r($arr1);
+
+
+$arr1 = array();
+$arr1 = [
+    1 => 'Ivanov',
+    3 => 'Petrov',
+];
+
+$arr1[5] = 'Sidorov';
+$arr1[] = 'Kadrov';
+$arr1[] = 10;
+$arr1["9"] = 7.8;
+
+echo '<pre>' . print_r($arr1, 1) . '</pre>';
+
+echo $arr1[6];
+echo $arr1[2];
+
+
+$array = array(
+    1    => "a",
+    "1"  => "b",
+    1.5  => "c",
+    true => "d",
+);
+
+var_dump($array);
+
+$goods = [
+    [
+        'title' => 'Nokia',
+        'price' => 100,
+        'qty' => 10,
+    ],
+    [
+        'title' => 'Sony',
+        'price' => 98,
+        'qty' => 110,
+    ],
+
+
+];
+
+echo '<pre>' . print_r($goods, 1) . '</pre>';
+//echo "<h3>{$goods['title']}</h3>";
+//echo $goods[1]['title'];
+*/
+//$nums  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,];
+for ($q = 1; $q < 11; $q++) {
+    $nums[] = $q;
+}
+
+echo '<pre>' . print_r($nums, 1) . '</pre>';
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-?>;
+?>
