e1c5f9b3d6be7cba33467b19f7ffc8d892db3448
@phpsturm <vla78dan@gmail.com>
2024-12-27 17:33:35 +0300

    sok10_3_20-34

--- kud_lern/22_arrays/22_arrays.php
+++ kud_lern/22_arrays/22_arrays.php
@@ -1,4 +1,70 @@
 <?php error_reporting(E_ALL);
 
-$nums  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,];
+$nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,];
+/*
+$q = 0;
+while ($q < 10) {
+    echo $nums[$q] . "\n";
+    $q++;
+}
+
+for ($q = 0; $q < 11; $q++) {
+    echo $nums[$q] . "\n";
+}
+
+foreach ($nums as $num) {
+    echo $num . '<br>';
+}
+
+foreach ($nums as $key => $value) {
+    echo "Key: $key, Value: $value<br>";
+}*/
+
+$goods = [
+    [
+        'title' => 'Nokia',
+        'price' => 100,
+        'qty' => 10,
+    ],
+    [
+        'title' => 'Sony',
+        'price' => 98,
+        'qty' => 110,
+    ],
+    [
+        'title' => 'LG',
+        'price' => 125,
+        'qty' => 44,
+    ],
+
+
+];
+
+echo '<pre>' . print_r($goods, 1) . '</pre>';
+
+//for ($i = 0; $i < 3; $i++) {
+//    echo "Наименование: {$goods[$i]['title']}. <br>";
+//    echo "Цена: {$goods[$i]['price']}. <br>";
+//    echo "Количество: {$goods[$i]['qty']}. <br>";
+//    echo '<hr>';
+//}
+
+foreach ($goods as $good) {
+    echo "Наименование: {$good['title']}. <br>";
+    echo "Цена: {$good['price']}. <br>";
+    echo "Количество: {$good['qty']}. <br>";
+    echo '<hr>';
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ php_sok/index.php
@@ -0,0 +1,8 @@
+<?php error_reporting(E_ALL);
+
+$array = array('Bob, Dodd, Modd',);
+
+print_r($array);
+//
+//echo "Hello Sok!";
+?>
