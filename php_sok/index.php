8ee5a9f94c7cd11702655f26ee515df924bafb7a
@phpsturm <vla78dan@gmail.com>
2024-12-27 22:32:00 +0300

    sok10_3_end

--- php_sok/index.php
+++ php_sok/index.php
@@ -1,8 +1,65 @@
 <?php error_reporting(E_ALL);
 
-$array = array('Bob, Dodd, Modd',);
 
-print_r($array);
+//$array = array(
+//    'name1' => 'Bob',
+//    'name2' => 'Dodd',
+//    'name3' => 'Modd',
+//);
 //
-//echo "Hello Sok!";
+//print_r($array);
+
+//empty(); empty — Проверяет, пуста ли переменная
+//isset($name); isset — Определяет, объявили ли переменную и отличается ли её значение от null
+
+//if (empty($name)) {
+//    echo "Есть значение";
+//} else {
+//    echo "Not значение";
+//}
+/*
+switch ($name) {
+    case 'Alex':
+    case 'Petr':
+        echo "Это либо Petr либо Alex";
+        break;
+    case 'Bob':
+        echo " This is Bob";
+        break;
+    default:
+        echo "Не найдено";
+        break;
+}
+*/
+
+$name = 'Bob';
+function show_name($name)
+{
+    echo $name;
+   }
+
+show_name($name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
