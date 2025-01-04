087a76defce900b456deed60b7f1b279d9d7b3f0
@phpsturm <vla78dan@gmail.com>
2025-01-04 23:52:40 +0300

    K_php8_25_Function_17-59

---
+++ kud_lern_php8/25_function/25_function.php
@@ -0,0 +1,93 @@
+<?php error_reporting(-1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ *
+ *
+ *
+ *
+ *
+ *function sum( $a,  $b,  $c) :float
+{
+    return $a + $b + $c;
+}
+
+var_dump(sum(1, 2.7, 3));
+ *
+ *function sum(int $a, int $b, int $c)
+{
+    return $a + $b + $c;
+}
+
+$res = sum(1,2.7,3);
+var_dump($res);
+ *
+ *function test()
+{
+
+}
+var_dump(test());
+ *
+ *function sum( $a,  $b,  $c) :int
+{
+echo $a + $b + $c;
+}
+
+sum(1, 2.7, 3);
+ *
+ *function sum(int $a, int $b, int $c): void
+{
+echo $a + $b + $c;
+}
+
+sum(1, 2.5, 3);
+ *
+ *function test()
+{
+    static $a = 0;
+    echo $a;
+    $a++;
+}
+
+
+test();
+test();
+test();
+ *
+ *function sum($a, $b, ...$nums)
+{
+    $res = $a * $b;
+    foreach ($nums as $num) {
+        $res += $num;
+    }
+    echo $res;
+}
+
+sum(1, 2, 3, 4, 5);
+ */
