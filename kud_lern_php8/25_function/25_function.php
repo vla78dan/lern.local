98fa9dfd2f7005ba122c08b3fecf7f1ed16f7a4e
@phpsturm <vla78dan@gmail.com>
2025-01-05 00:32:38 +0300

    K_php8_25_Function_end

--- kud_lern_php8/25_function/25_function.php
+++ kud_lern_php8/25_function/25_function.php
@@ -1,27 +1,10 @@
 <?php error_reporting(-1);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function sum( $term1,  $term2 = 1,  $factor = 2)
+{
+    return ($term1 + $term2) * $factor;
+}
+echo sum(term1: 1, term2: 2,  factor: 3);
 
 
 
@@ -29,9 +12,23 @@
 
 /*
  *
+ *function sum( $term1,  $term2 = 1,  $factor = 2)
+{
+    return ($term1 + $term2) * $factor;
+}
+echo sum(1, factor: 3);
  *
- *
- *
+ *$a = '';
+function test(string $a): ?string
+{
+    if ($a) {
+        return 'Test';
+    }
+    return null;
+
+
+}
+
  *
  *function sum( $a,  $b,  $c) :float
 {
